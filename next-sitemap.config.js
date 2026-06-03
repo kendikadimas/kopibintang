9502c5f80dcad03747a7f2c23ab202b3cba47707
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-06-03 11:29:14 +0700

    config: change sitemap filename to sitemap_main.xml to resolve GSC cache bug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
   siteUrl: 'https://kopibintangpringsewu-official.com',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
+  sitemapBaseFileName: 'sitemap_main',
   outDir: 'out',
   trailingSlash: true,
 }
